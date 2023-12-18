4115b459b3f98856243c80ac19eae41f59312580
WhatsAppIT <it.technology@internet.ru>
2023-12-18 15:03:04 +0400

    user Schema

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const isEmail = require("validator/lib/isEmail");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: [2, "минимальная длинна 2 символа"],
+      maxlength: [30, "максимальная длинна 30 символов"],
+      default: "Имя пользователя",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => isEmail(email),
+        message: "Неправильный формат почты",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
